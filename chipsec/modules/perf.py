678f62363c720a44623bc55499452744c782704b
hcwang <hwang11@lenovo.com>
2017-09-13 15:37:23 +0800

    Added prefetchers check support.

--- chipsec/modules/perf.py
+++ chipsec/modules/perf.py
@@ -41,6 +41,11 @@ Checks if performance registers are correct.
 from chipsec.module_common import *
 
 IOUT_SLOPE_DEFAULT = 0x200
+IOUT_SLOPE_OK = 0
+IOUT_SLOPE_WARN = 1
+
+PREFETCHERS_DISABLED = 0
+PREFETCHERS_ENABLED = 1
 
 # ############################################################
 # SPECIFY PLATFORMS THIS MODULE IS APPLICABLE TO
@@ -61,17 +66,52 @@ class perf(BaseModule):
     def check_iout_slope(self):
         vr_misc_config = self.cs.read_register( 'VR_MISC_CONFIG' )
         self.cs.print_register( 'VR_MISC_CONFIG', vr_misc_config )
+        
+        iout_slope = self.cs.read_register_field( 'VR_MISC_CONFIG', 'IOUT_SLOPE', True )
+        if iout_slope > IOUT_SLOPE_DEFAULT:
+            self.logger.log_warn_check( "IOUT_SLOPE could be not equal to default for better performance." )
+            return IOUT_SLOPE_WARN
+        else:
+            return IOUT_SLOPE_OK
 
-        return self.cs.read_register_field( 'VR_MISC_CONFIG', 'IOUT_SLOPE', True )
+    def check_prefetchers(self):
+        prefetchers = self.cs.read_register( 'Prefetchers' )
+        self.cs.print_register( 'Prefetchers', prefetchers )
+
+        prefetchers_en = PREFETCHERS_ENABLED
+        
+        hw_prefetchers = self.cs.read_register_field( 'Prefetchers', 'HardwarePrefetcher', True )
+        acl_prefetchers = self.cs.read_register_field( 'Prefetchers', 'AdjacentCacheLinePrefetch', True )
+        dcu_streamer_prefetchers = self.cs.read_register_field( 'Prefetchers', 'DCUStreamerPrefetcher', True )
+        dcu_ip_prefetchers = self.cs.read_register_field( 'Prefetchers', 'DCUIpPrefetcher', True )
+        
+        if hw_prefetchers != 0:
+            self.logger.log_failed_check( "Hardware prefetcher is disabled!" )
+            prefetchers_en = PREFETCHERS_DISABLED
+        elif acl_prefetchers != 0:
+            self.logger.log_failed_check( "Adjacent cache line prefetch is disabled!" )
+            prefetchers_en = PREFETCHERS_DISABLED
+        elif dcu_streamer_prefetchers != 0:
+            self.logger.log_failed_check( "DCU streamer prefetcher is disabled!" )
+            prefetchers_en = PREFETCHERS_DISABLED
+        elif dcu_ip_prefetchers != 0:
+            self.logger.log_failed_check( "DCU IP prefetcher is disabled!" )
+            prefetchers_en = PREFETCHERS_DISABLED
+        else:
+            self.logger.log_passed_check( "All prefetchers are enabled." )
+
+        return prefetchers_en
 
     def check_perf_cfg( self ):
         self.logger.start_test( "Performance Registers Check" )
 
-        iout_slope= self.check_iout_slope()
-        
-        if iout_slope > IOUT_SLOPE_DEFAULT:
+        prefetchers_res = self.check_prefetchers()
+        iout_slope_res = self.check_iout_slope()
+
+        if prefetchers_res != PREFETCHERS_ENABLED:
+            res = ModuleResult.FAILED
+        elif iout_slope_res != IOUT_SLOPE_OK:
             res = ModuleResult.WARNING
-            self.logger.log_warn_check( "IOUT_SLOPE could be not equal to default for better performance." )
         else:
             res = ModuleResult.PASSED
             self.logger.log_passed_check( "All registers are set in performance spec" )
